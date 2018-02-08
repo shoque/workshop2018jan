d585db331b92fb3ba24dcdd0237792faea9c4936
shamiul hoque <shoq9876@gmail.com>
2018-02-08 14:59:34 +0600

    javscript seprated to a different file

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+$(function() {
+$("#about").hide();
+    $("#about-item").click(function () {
+      $("#home").hide();
+      $("#about").show();
+    });
+    $("#home-item").click(function () {
+      $("#about").hide();
+      $("#home").show();
+    });
+
+});
