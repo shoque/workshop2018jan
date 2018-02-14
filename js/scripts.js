f05176f605b3f7bc86ae4fc91b143033115c7085
shamiul hoque <shoq9876@gmail.com>
2018-02-14 16:59:28 +0600

    jquery html loading added,book add functionality added

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,49 @@
 
-w3.includeHTML();
-$(function () {
-    $("#sidenav").sideNav();
-    $("#about").hide();
 
- 
-    $("#about-item").click(function () {
-        $("#home").hide();
-        $("#about").show();
-    });
-    $("#home-item").click(function () {
+$(function () {
+
+    $("#menu").load("./html/nav.html",function(){
+        $("#about").load("./html/about.html",function(){
+
+        
+        }); 
+        $("#home").load("./html/home.html",function(){
+         
+
+        }); 
+        $("#book-add").load("./html/book-add.html",function(){
+            console.log("book add loaded");
+          
+
+        }); 
+
+        $("#sidenav").sideNav();
         $("#about").hide();
-        $("#home").show();
-    });
+        $("#book-add").hide();
+     
+     
+        $("#about-item").click(function () {
+            $("#home").hide();
+            $("#about").show();
+        });
+        $("#home-item").click(function () {
+            $("#about").hide();
+            $("#home").show();
+        });
+
+        $("#add-book-item").click(function () {
+            $("#home").hide();
+            $("#about").hide();
+            $('.side-nav').sideNav('hide');
+            $("#book-add").show();
+        });
+
+
+    }); 
+
+
+
+
+
 
 });
